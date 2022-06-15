82d2c7d4438c371807ea1eb56186f4122f7dcc43
TortillaZHawaii <dwysocki157@gmail.com>
2022-06-15 15:34:56 +0200

    Fix nvprof test

--- tests/nvprof_test.sh
+++ tests/nvprof_test.sh
@@ -6,7 +6,11 @@ for i in {1..9}
 do
     for j in {1..9}
     do
-        echo "$i $j" 
-        (nvprof --csv ./build/cublurhash $i $j $1) >> $2 2>&1
+        for k in {0..2}
+        do
+            echo "$i $j"
+            echo "\"n_log\",\"$1\",\"$i\",\"$j\"" >> $2
+            (nvprof --csv --normalized-time-unit ms ./build/cublurhash $i $j $1) >> $2 2>&1
+        done
     done
 done
