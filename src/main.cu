d4ef4d9f33215698544eddf4a50968b0202cf2bf
TortillaZHawaii <dwysocki157@gmail.com>
2022-06-28 16:31:08 +0200

    Add analytic files to .gitignore

--- src/main.cu
+++ src/main.cu
@@ -29,14 +29,14 @@ int main(int argc, const char **argv)
         return 1;
     }
 
-    std::cout << "Loading image..." << std::endl;
+    //std::cout << "Loading image..." << std::endl;
 
     const char *filename = argv[3];
     CuBlurHash::Image image = CuBlurHash::Image(filename);
 
-    std::cout << "Image loaded." << std::endl;
+    //std::cout << "Image loaded." << std::endl;
 
-    std::cout << "Encoding image..." << std::endl;
+    //std::cout << "Encoding image..." << std::endl;
 
     std::clock_t start_time = std::clock();
 
@@ -46,19 +46,19 @@ int main(int argc, const char **argv)
 
     int duration_ms = (end_time - start_time) * 1000 / CLOCKS_PER_SEC;
 
-    std::cout << "Image encoded in " << duration_ms << " ms." << std::endl;
+    //std::cout << "Image encoded in " << duration_ms << " ms." << std::endl;
 
     if(argc == 5) 
     {
         std::string csv_file_path = std::string(argv[4]);
-        std::cout << "Writing encoder logs to " << csv_file_path << std::endl;
+        //std::cout << "Writing encoder logs to " << csv_file_path << std::endl;
 
         CuBlurHash::CsvLogger(csv_file_path, ">")
             .log_encoding_results(std::string(filename), image, hash,
                 "Thrusted", x_components, y_components, duration_ms);
     }
 
-    std::cout << "Hash: " << std::endl;
+    //std::cout << "Hash: " << std::endl;
     std::cout << hash << std::endl;
 
     return 0;
